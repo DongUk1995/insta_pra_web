100bcc84a50137b3a55b7ef1fbef44e939ba0ce9
김동욱 <email@example.com>
2024-09-06 10:43:58 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/potato">
+            <h1>tomato</h1>
+          </Route>
+          <Route path="/bana">
+            <h1>baaa</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
