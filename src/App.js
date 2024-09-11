db7d8cf70098be8c730f135758162a232f3f3c87
김동욱 <email@example.com>
2024-09-11 11:50:25 +0900

    css_web

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 import { useReactiveVar } from "@apollo/client";
-import { isLoggedInvar } from "./apollo";
+import { darkModeVar, isLoggedInvar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInvar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          {isLoggedIn ? <Home /> : <Login />}
-        </Route>
-        <NotFound />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 export default App;
